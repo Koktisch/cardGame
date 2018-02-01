58399e3c2b0132b314511715236a189102f403f5
Przemyslaw Koprowski <koprowski.przemyslaw@gmail.com>
2018-02-02 00:31:05 +0100

    Final application structure

--- client/Board.js
+++
@@ -1,9 +0,0 @@
-var socket = io();
-
-socket.on('startGame', function (data) {
-    socket.emit('setPriority', { hostGame: host });
-});
-
-socket.on('setPriority', function (data) {
-    $('#priority').html(data.starting);
-});
