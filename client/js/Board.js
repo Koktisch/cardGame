9dc73bc3271d8ed2c54bd04993b9883207ed79f0
Przemyslaw Koprowski <koprowski.przemyslaw@gmail.com>
2018-02-02 00:17:25 +0100

    Moving Board.js into one folder

--- client/js/Board.js
+++ client/js/Board.js
@@ -1,3 +1,12 @@
+var socket = io();
+
+socket.on('startGame', function (data) {
+    socket.emit('setPriority', { hostGame: host });
+});
+
+socket.on('setPriority', function (data) {
+    $('#priority').html(data.starting);
+});
 socket.on('startGameBoard', function (startGameData) {
     $('#board').css('display', 'block');
     $('#priority').html(startGameData.startTxt);
